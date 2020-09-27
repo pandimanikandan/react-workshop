ad0e5b994e51df7f96b31dc1d7e485f8e40b0d39
pandimanikandan <39363714+pandimanikandan@users.noreply.github.com>
2020-09-28 00:40:23 +0530

    Updates

---
+++ precache-manifest.5e7852c45a185fcf3a43d96ee246270b.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4252f9e95288fe973b0a3e26b73dab5d",
+    "url": "/react-workshop/index.html"
+  },
+  {
+    "revision": "7a654751e95b6d41e867",
+    "url": "/react-workshop/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "49701c1331b3ff4ab0d8",
+    "url": "/react-workshop/static/css/main.43e024be.chunk.css"
+  },
+  {
+    "revision": "7a654751e95b6d41e867",
+    "url": "/react-workshop/static/js/2.49cee534.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-workshop/static/js/2.49cee534.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49701c1331b3ff4ab0d8",
+    "url": "/react-workshop/static/js/main.1860fffd.chunk.js"
+  },
+  {
+    "revision": "28969ed021637d9fd097",
+    "url": "/react-workshop/static/js/runtime-main.223fd330.js"
+  },
+  {
+    "revision": "d5f2a7f493e94ed267d41620eef143d9",
+    "url": "/react-workshop/static/media/poster1.d5f2a7f4.jpg"
+  },
+  {
+    "revision": "81ccf144677486906fcf751450652895",
+    "url": "/react-workshop/static/media/poster2.81ccf144.jpg"
+  },
+  {
+    "revision": "e242f0372e7bdbf2e48aa5afefc72b97",
+    "url": "/react-workshop/static/media/poster3.e242f037.jpg"
+  },
+  {
+    "revision": "56f097245c605a5d312f103dedb21bcb",
+    "url": "/react-workshop/static/media/poster4.56f09724.jpg"
+  },
+  {
+    "revision": "5ef7f79461c7a8c0a95652b813fb4722",
+    "url": "/react-workshop/static/media/poster5.5ef7f794.jpg"
+  },
+  {
+    "revision": "164b05a3cd770650dd30bc71f5407e99",
+    "url": "/react-workshop/static/media/poster6.164b05a3.jpg"
+  },
+  {
+    "revision": "23ac494a92660204d6b8acbba20aa0e9",
+    "url": "/react-workshop/static/media/poster7.23ac494a.jpg"
+  },
+  {
+    "revision": "ef89f612f6e3d684892d6525b7957ce4",
+    "url": "/react-workshop/static/media/poster8.ef89f612.jpg"
+  },
+  {
+    "revision": "65d6d5002a2f98860f29d3c61599c5ac",
+    "url": "/react-workshop/static/media/poster9.65d6d500.jpg"
+  }
+]);
